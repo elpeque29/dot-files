68f31d53fe6a33f3975607aa3e4d05234f944d79
elpeque29 <117090588+elpeque29@users.noreply.github.com>
2025-04-12 15:07:26 -0300

    Adding crucible by typecraft-dev

---
+++ crucible/dotfiles-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ORIGINAL_DIR=$(pwd)
+
+# To-do: write this script xD
---
+++ crucible/install-flatpaks.sh
@@ -0,0 +1,14 @@
+FLATPAKS=(
+  "com.spotify.Client"
+  "com.obsproject.Studio"
+  "es.danirod.Cartero"
+)
+
+for pak in "${FLATPAKS[@]}"; do
+  if ! flatpak list | grep -i "$pak" &> /dev/null; then
+    echo "Installing Flatpak: $pak"
+    flatpak install --noninteractive "$pak"
+  else
+    echo "Flatpak already installed: $pak"
+  fi
+done
