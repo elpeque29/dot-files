1f34114a843b4b4106c9743e3709595c419f767c
elpeque29 <117090588+elpeque29@users.noreply.github.com>
2025-04-23 01:07:26 -0300

    Wrote dotfiles-setup.sh

--- crucible/dotfiles-setup.sh
+++ crucible/dotfiles-setup.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
-ORIGINAL_DIR=$(pwd)
+GIT_DIR=https://github.com/elpeque29/dot-files
+REPO_NAME=dot-files
 
-# To-do: write this script xD
+echo "Cloning repo into the temporary directory..."
+
+cd /tmp
+CURRENT_DIR=$(pwd)
+
+if [ "$CURRENT_DIR" = "/tmp" ]; then
+
+  git clone $GIT_DIR
+
+  echo "Copying files..."
+
+  cp -r $REPO_NAME/.config /home/$USER/
+  cp -r $REPO_NAME/.themes /home/$USER/
+  cp -r $REPO_NAME/Pictures/ /home/$USER/Pictures/
+  cp $REPO_NAME/.zshrc /home/$USER/
+  cp $REPO_NAME/.ideavimrc /home/$USER/
+
+  if [ "$USER" = "peque" ]; then
+    mv /home/$USER/.config/.gitconfig /home/$USER/
+  fi
+
+  echo "Deleting temporary files"
+
+  rm -rf /tmp/$REPO_NAME
+
+fi
+
+echo "Done"
